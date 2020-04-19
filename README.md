9747fc7b1ce0a8be67f030d96fe96a8ef9e87ab0
paulito-bandito <paul.walter.software@gmail.com>
2020-04-19 13:17:33 -0400

    Initial check-in.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Data Structures Homework
+
+Selected Solutions to the MIT course 6.006 "Introduction to Algorithms"
+- Course home: https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/index.htm
+- Textbook: \"Introduction To Data Structures\" by Charles E. Leiserson, Clifford Stein, Ronald Rivest, and Thomas H. Cormen (CLRS).
+
+## Class Notes
+Check out the generated HTML file in the `.docs/MIT Data Structures` folder. I'll update this periodically.
+
+## Table of Contents
+1) tba
+2) tba
+3) tba
+4) Divide and Conquer (in folder `src/com/walterp/datastruct/four`)
---
+++ src/com/walterp/datastruct/four/MaxSubArraySum_DivideAndConquer.ts
@@ -0,0 +1,96 @@
+namespace com.walterp.datastruct.four{
+    class SubArrayInfo{
+        /**
+         * @param arr
+         * @param low
+         * @param high
+         * @param sum
+         */
+        constructor(  arr, low:number, high:number, sum:number ){
+
+        }
+    }
+
+    const
+        /**
+         * Summs up the segment of an array as defined by the "low" and "high" indices.
+         *
+         * @param {Array} arr       The array of deltas we wish to inspect
+         * @param {int} low         The starting index of the array segment we wish to inspect.
+         * @param {int} high        The ending index of the array segment we wish to inspect.
+         */
+        maxCrosSubArray = ( arr, low, middle, high ):SubArrayInfo =>{
+
+            let left_sum:number = Number.MIN_SAFE_INTEGER,
+                right_sum:number =  Number.MIN_SAFE_INTEGER,
+                sum:number = 0,
+                max_left:number = 0;
+
+            for (let i = middle; i>-1; i--){
+
+                sum = sum + arr[i];
+
+                if(sum > left_sum){
+                    left_sum = sum;
+                    max_left = i;
+                }
+            }
+
+            for (let j = middle + 1; j < high + 1; j++){
+
+                sum = sum + arr[j];
+
+                if(sum > left_sum){
+                    right_sum = sum;
+                    max_left = j;
+                }
+            }
+
+
+            // return .max-left; max-right; left-sum + right-sum/
+            return new SubArrayInfo( arr, max_left, max_right, (left_sum + right_sum) );
+
+        },
+        /**
+         * This method will chunk up the sub arrays, find the greatest sum, and then return that answer.
+         *
+         * @param {Array}   arr       The array of deltas we wish to inspect
+         * @param {int}     low       The starting index of the array segment we wish to inspect.
+         * @param {int}     high      The ending index of the array segment we wish to inspect.
+         *
+         * @return {SubArrayInfo}
+         */
+        maxSubArray = ( arr, low, high ): SubArrayInfo =>{
+
+            // base case
+            if ( l = h ){
+                return new SubArrayInfo( l, h, arr[l] );
+            }
+
+            // recursive case
+            const 	middle:Number = ( (low + high) / 2 ),
+                left:SubArrayInfo  = maxSubArray(arr, low, middle),
+                right:SubArrayInfo = maxSubArray(arr, middle + 1, high),
+                cross:SubArrayInfo = maxCrosSubArray( arr, low, middle, high );
+
+            // Return the greatest sum.
+            if( ( left.sum >= right.sum ) && ( left.sum >= cross.sum ) ){
+                return new SubArrayInfo( l, h, arr[l] );
+            }
+
+            /*
+             * if left-sum 􏰃 right-sum and left-sum 􏰃 cross-sum
+             return .left-low; left-high; left-sum/
+             elseif right-sum 􏰃 left-sum and right-sum 􏰃 cross-sum
+             return .right-low; right-high; right-sum/ else return .cross-low; cross-high; cross-sum/
+             */
+
+        },
+        arr = [-2, -5, 6, -2, -3, 1, 5, -6];
+
+
+    maxSubArray(arr, 0, arr.length -1 );
+}
+
+
+
